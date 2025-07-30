a05d32d0217f289e992d4edbb2dbda2c818662db
Nemanja <nradovanovic12022rn@raf.rs>
2025-07-30 16:20:43 +0200

    Button | Add Link Button Hover Animation

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,16 +6,16 @@ import { cn } from "@/utils/utils.ts"
 import ArrowUpRightIcon from "@/assets/icons/ArrowUpRight.tsx";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-full transition-all outline-none hover:cursor-pointer",
+  "relative inline-flex items-center justify-center whitespace-nowrap rounded-full transition-all outline-none hover:cursor-pointer group",
   {
     variants: {
       variant: {
         default:
           "bg-gray-850 text-normal-14 text-gray-100 hover:bg-gray-700 hover:shadow-small",
         navigation:
-          "bg-gray-850 text-normal-14 text-gray-100 hover:bg-gray-700",
+          "bg-transparent text-normal-14 text-gray-100 hover:bg-gray-700",
         link:
-          "bg-transparent text-normal-14 text-gray-100 hover:bg-gray-750"
+          "bg-transparent text-normal-14 text-gray-100"
       },
       size: {
         default: "h-8 px-5 py-1.5",
@@ -52,6 +52,7 @@ const Button = ({
       {...props}>
       {children}
       {variant == "link" && <ArrowUpRightIcon/>}
+      {variant == "link" && <div className="absolute w-0 h-0.5 bottom-0 left-1/2 -translate-x-1/2 rounded-full bg-gray-300 group-hover:w-[calc(100%-28px)] group-hover:-translate-x-[calc(50%-2px)] transition-all duration-500"/>}
     </Comp>
   )
 }
